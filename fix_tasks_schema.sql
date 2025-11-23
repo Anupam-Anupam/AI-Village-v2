004616861cf89374e31fc9d7949f975ed3d3b696
Anupam-Anupam <anupamchettimada@yahoo.ca>
2025-11-22 19:09:33 -0500

    removing redundant stuff

--- fix_tasks_schema.sql
+++
@@ -1,5 +0,0 @@
--- Fix tasks table to allow NULL agent_id
--- Tasks are created without an agent, agents claim them when they pick them up
-
-ALTER TABLE tasks ALTER COLUMN agent_id DROP NOT NULL;
-
